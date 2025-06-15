6cb66211f86ea5da5200b1266fa0b86c6021b5e5
Ivan Saydyashev <ivasay@vk.com>
2025-06-15 20:34:36 +0300

    init

---
+++ MacCalc/MacCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCalcApp.swift
+//  MacCalc
+//
+//  Created by Ivan Saydyashev on 15.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MacCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacCalcTests/MacCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacCalcTests.swift
+//  MacCalcTests
+//
+//  Created by Ivan Saydyashev on 15.06.2025.
+//
+
+import Testing
+@testable import MacCalc
+
+struct MacCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
