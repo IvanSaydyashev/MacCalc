4c4f83524d069e1e357ea4ac442a8ca74c14111b
Ivan Saydyashev <ivasay@vk.com>
2025-06-16 12:05:44 +0300

    Calculator 1.0

--- MacCalc/ContentView.swift
+++ MacCalc/ContentView.swift
@@ -8,16 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var task = "0"
+    
+    let columns = Array(repeating: GridItem(.flexible()), count: 4)
+    let buttons = ["7", "8", "9", "÷", "4", "5", "6", "×", "1", "2", "3", "-", "0", "C", "=", "+"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            Text(task).font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            LazyVGrid(columns: columns){
+                ForEach(buttons, id: \.self){ label in
+                    Button(label){
+                        if label.allSatisfy({$0.isNumber}){
+                            if task == "0" || task == "Error"{
+                                task = label
+                            } else if task.count < 18{
+                                task += label
+                            }
+                        } else if label == "C"{
+                            task = "0"
+                        } else if label == "="{
+                            let format = task.replacingOccurrences(of: "×", with: "*").replacingOccurrences(of: "÷", with: "/")
+                            let expr = NSExpression(format: format)
+                            let result = expr.expressionValue(with: nil, context: nil) as? NSNumber
+                            task = String(describing: result ?? 0)
+                            if task == "0"{
+                                task = "Error"
+                            }
+                        } else if task.count < 17{
+                            if let last = task.last, "+-×÷".contains(last) == false {
+                                task += label
+                            }
+                        }
+                        
+                    }.font(.title).padding()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.gray.opacity(0.2))
+                        .buttonStyle(PlainButtonStyle())
+                        .contentShape(Rectangle())
+                        
+                }
+            }
+            Button(action: {
+                task.removeLast()
+                if task.isEmpty {
+                    task = "0"
+                }
+            }) {
+                Image(systemName: "delete.left")
+                    .font(.title)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    
+            }.frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.2))
+            .buttonStyle(PlainButtonStyle())
+            .contentShape(Rectangle())
+        }.padding(50)
     }
+    
 }
+
+
 
 #Preview {
     ContentView()
